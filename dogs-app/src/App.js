93c3aa2e8a3d6ae26367ba5665c879b2dcc0f32e
Charlie Hewis <charlie.hewis@gmail.com>
2023-09-03 00:49:09 +0100

    Added theme for button

--- dogs-app/src/App.js
+++ dogs-app/src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
+import { ThemeProvider } from "@mui/material/styles";
 import { DogDisplay } from "./DogDisplay.tsx";
+import theme from "./Theme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Dogs App!</h1>
-      </header>
-      <DogDisplay />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Dogs App!</h1>
+        </header>
+        <DogDisplay />
+      </div>
+    </ThemeProvider>
   );
 }
 
