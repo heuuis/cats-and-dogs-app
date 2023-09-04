c3877afea7e5d658c9756b9ea93a9d1d1276e4aa
Charlie Hewis <charlie.hewis@gmail.com>
2023-09-04 22:44:52 +0100

    Created new pages

---
+++ src/cats/Cats.tsx
@@ -0,0 +1,12 @@
+import { CatImageDisplay } from "../image-display/CatImageDisplay";
+
+export const Cats = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cats!</h1>
+      </header>
+      <CatImageDisplay />
+    </div>
+  );
+};
---
+++ src/dogs/Dogs.tsx
@@ -0,0 +1,12 @@
+import { DogImageDisplay } from "../image-display/DogImageDisplay";
+
+export const Dogs = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dogs!</h1>
+      </header>
+      <DogImageDisplay />
+    </div>
+  );
+};
