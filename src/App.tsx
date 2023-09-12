bd27a228a1f9feedef0a2a43ec2f4be90122104a
Charlie Hewis <charlie.hewis@gmail.com>
2023-09-12 10:43:30 +0100

    Added basic home page and nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Tournament } from "./tournament/tournament";
 import { Cats } from "./cats/cats";
 import { Dogs } from "./dogs/dogs";
+import { Home } from "./home";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Tournament />} />
-      <Route path="cats" element={<Cats />} />
-      <Route path="dogs" element={<Dogs />} />
-    </Routes>
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/cats">Cats</Link>
+        <Link to="/dogs">Dogs</Link>
+        <Link to="/tournament">Tournament</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cats" element={<Cats />} />
+        <Route path="/dogs" element={<Dogs />} />
+        <Route path="/tournament" element={<Tournament />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/home.tsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cats and Dogs App!</h1>
+      </header>
+      <body>Use the nav bar to choose a page</body>
+    </div>
+  );
+};
