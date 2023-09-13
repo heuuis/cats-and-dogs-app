c50fee655b3df114519058c6184e971f351f6dee
Charlie Hewis <charlie.hewis@gmail.com>
2023-09-13 17:33:52 +0100

    Clickable sidebar with hover autoenabled on mouse leave

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,11 @@ import {
   faTrophy,
 } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
 
 function App() {
+  const [hoverDisabled, setHoverDisabled] = useState(false);
+
   const openGithubInNewWindow = () => {
     window.open("https://github.com/heuuis/cats-and-dogs-app", "_blank");
     return null;
@@ -22,13 +25,16 @@ function App() {
 
   return (
     <div className="App">
-      <nav className="navbar">
+      <nav
+        className={`navbar ${hoverDisabled ? "disable-hover" : ""}`}
+        onMouseLeave={() => setHoverDisabled(false)}
+      >
         <ul className="navbar-nav">
-          <li className="logo">
-            <Link to="/" className="nav-link">
+          <li className="logo" onClick={() => setHoverDisabled(!hoverDisabled)}>
+            <div className="nav-link">
               <span className="link-text">Heuuis</span>
               <FontAwesomeIcon icon={faAnglesRight} size="3x" />
-            </Link>
+            </div>
           </li>
           <li className="nav-item">
             <Link to="/" className="nav-link">
