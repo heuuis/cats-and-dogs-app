5c18e9eead6fb718a9a69a52d3bf1e56429424bb
Charlie Hewis <charlie.hewis@gmail.com>
2023-09-14 11:05:25 +0100

    Updated home page

--- src/home.tsx
+++ src/home.tsx
@@ -1,10 +1,35 @@
+import { faCat, faDog, faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
 export const Home = () => {
   return (
-    <div className="App">
+    <div className="App Home">
       <header className="App-header">
         <h1>Cats and Dogs App!</h1>
       </header>
-      <body>Use the nav bar to choose a page</body>
+      <body>
+        <ul>
+          <li>
+            <Link to="/cats">
+              <FontAwesomeIcon icon={faCat} size="5x" />
+              <div>Cats</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/dogs">
+              <FontAwesomeIcon icon={faDog} size="5x" />
+              <div>Dogs</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/tournament">
+              <FontAwesomeIcon icon={faTrophy} size="5x" />
+              <div>Tournament</div>
+            </Link>
+          </li>
+        </ul>
+      </body>
     </div>
   );
 };
