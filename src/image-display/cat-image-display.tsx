516b49c5dc07ed014071d922fb886b42cf780730
Charlie Hewis <charlie.hewis@gmail.com>
2023-09-15 10:00:17 +0100

    Updated Tournament page

--- src/image-display/cat-image-display.tsx
+++ src/image-display/cat-image-display.tsx
@@ -1,8 +1,6 @@
+import { catApiKey, catApiUrl } from "../values";
 import { AnimalImageDisplay } from "./animal-image-display";
 
-const catApiKey =
-  "live_oWZmpYEWR0WIqJtXXGJrZDYMu7K2Ulmjgy1hEB9pHCTubTxxdbdV6ABT6lJcZKWr";
-const catApiUrl = "https://api.thecatapi.com";
 const getCatImagesQuery = (n: number) => {
   return `${catApiUrl}/v1/images/search?limit=${n}&api_key=${catApiKey}`;
 };
--- src/image-display/dog-image-display.tsx
+++ src/image-display/dog-image-display.tsx
@@ -1,8 +1,6 @@
+import { dogApiKey, dogApiUrl } from "../values";
 import { AnimalImageDisplay } from "./animal-image-display";
 
-const dogApiKey =
-  "live_mrlCAiUTEa97Yw0bIXLgim39NQBmepZBq0tBXZ7vYQwy93wUjE25nyYRuzYNlsch";
-const dogApiUrl = "https://api.thedogapi.com";
 const getDogImagesQuery = (n: number) => {
   return `${dogApiUrl}/v1/images/search?limit=${n}&api_key=${dogApiKey}`;
 };
