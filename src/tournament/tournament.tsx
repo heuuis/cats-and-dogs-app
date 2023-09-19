c4f828055bb67dd5c709791e4d9d2353dc95ceff
Charlie Hewis <charlie.hewis@gmail.com>
2023-09-19 09:29:57 +0100

    Tournament page functionality for showing animals

--- src/tournament/tournament.tsx
+++ src/tournament/tournament.tsx
@@ -6,6 +6,7 @@ import { catApiKey, catApiUrl, dogApiKey, dogApiUrl } from "../values";
 import { faCrown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "@mui/material";
+import { ImageDisplay } from "../image-display/image-display";
 
 export const Tournament = () => {
   // The tournament will be to find the cutest cat or dog
@@ -15,13 +16,18 @@ export const Tournament = () => {
   // cuterThanDogs[i] = list of cats cuter than i of the dogs
   // and vice versa for dogs
   // then, later, we may choose to add a feature to sort the lists too to get a total ordering
-  // when all the dogs and cats are in the lists cuterThanDogs and cuterThanCats we can give a score by summing i*cuterThanDogs[i].length for each
+  // when all the dogs and cats are in the lists cuterThanDogs and cuterThanCats we can give a
+  // score by summing i*cuterThanDogs[i].length for each
 
   const [cats, setCats] = useState([] as Image[]);
   const [dogs, setDogs] = useState([] as Image[]);
   const [cuterThanCats, setCuterThanCats] = useState([]);
   const [cuterThanDogs, setCuterThanDogs] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [catCounter, setCatCounter] = useState(0);
+  const [dogCounter, setDogCounter] = useState(0);
+  const [catImageRendered, setCatImageRendered] = useState(false);
+  const [dogImageRendered, setDogImageRendered] = useState(false);
 
   const [tournamentStarted, setTournamentStarted] = useState(false);
   useEffect(() => {
@@ -51,6 +57,24 @@ export const Tournament = () => {
     }
   }, [tournamentStarted]);
 
+  const handleCatImageLoad = () => {
+    setCatImageRendered(true);
+  };
+
+  const handleDogImageLoad = () => {
+    setDogImageRendered(true);
+  };
+
+  const handleCatClick = () => {
+    setCatImageRendered(false);
+    setCatCounter(catCounter + 1);
+  };
+
+  const handleDogClick = () => {
+    setDogImageRendered(false);
+    setDogCounter(dogCounter + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -60,10 +84,40 @@ export const Tournament = () => {
         {tournamentStarted ? (
           <div className="content-wrapper">
             <div className="left-half">
-              <CatImageDisplay />
+              {catCounter > 0 && catCounter >= cats.length ? (
+                <div className="image-container">That was your last cat!</div>
+              ) : (
+                <ImageDisplay
+                  imageUrl={cats[catCounter]?.url || ""}
+                  altText={"Cat"}
+                  onImageLoad={handleCatImageLoad}
+                />
+              )}
+              <Button
+                variant="outlined"
+                onClick={handleCatClick}
+                disabled={!catImageRendered}
+              >
+                Cat is cuter!
+              </Button>
             </div>
             <div className="right-half">
-              <DogImageDisplay />
+              {dogCounter > 0 && dogCounter >= dogs.length ? (
+                <div className="image-container">That was your last dog!</div>
+              ) : (
+                <ImageDisplay
+                  imageUrl={dogs[dogCounter]?.url || ""}
+                  altText={"Dog"}
+                  onImageLoad={handleDogImageLoad}
+                />
+              )}
+              <Button
+                variant="outlined"
+                onClick={handleDogClick}
+                disabled={!dogImageRendered}
+              >
+                Dog is cuter!
+              </Button>
             </div>
           </div>
         ) : (
@@ -75,7 +129,7 @@ export const Tournament = () => {
               }}
               color="inherit"
             >
-              <FontAwesomeIcon icon={faCrown} />
+              <FontAwesomeIcon icon={faCrown} size="6x" />
               <div>Begin Cute-off!</div>
             </Button>
           </div>
