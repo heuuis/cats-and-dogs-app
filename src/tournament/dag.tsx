d637944070e53455bd336fc91b80a9bc3684e0d9
Charlie Hewis <54192575+heuuis@users.noreply.github.com>
2023-09-24 00:46:33 +0100

    Updated dag to use ids

--- src/tournament/dag.tsx
+++ src/tournament/dag.tsx
@@ -21,11 +21,9 @@ export const createDAG = () => {
       return;
     }
     adjList[from].add(to);
-    updateAdjacencyList();
   };
 
   const doesCauseCycle = (from: string, to: string) => {
-    // console.log(`Checking if edge from ${from} to ${to} causes cycle`);
     // DFS to check for a cycle when adding the edge
     const visited: Set<string> = new Set();
 
@@ -43,133 +41,19 @@ export const createDAG = () => {
     return dfs(to);
   };
 
-  const getAllAnimalsLessCute = (node: string) => {
-    const allAnimalsLessCute: Set<string> = new Set();
-    const getAnimalsLessCute = (node: string) => {
-      adjList[node].forEach((childNode) => {
-        allAnimalsLessCute.add(childNode);
-        getAnimalsLessCute(childNode);
-      });
-    };
-    getAnimalsLessCute(node);
-    return allAnimalsLessCute;
-  };
-
-  const updateAdjacencyList = () => {
-    const nodes = getAllNodes();
-    nodes.forEach((node) => {
-      const animalType = node[0];
-      const adjacentAnimalType = animalType === "c" ? "d" : "c";
-      [...getAllAnimalsLessCute(node)]
-        .filter((x) => x[0] === adjacentAnimalType)
-        .forEach((childNode) => adjList[node].add(childNode));
-    });
-  };
-
   const getAllNodes = (): string[] => {
     return Object.keys(adjList);
   };
 
-  const getValidComparisons = (): [string, string][] => {
-    const allNodes = getAllNodes();
-    const cats = allNodes.filter((node) => node[0] === "c");
-    const dogs = allNodes.filter((node) => node[0] === "d");
-
-    const validEdges: [string, string][] = [];
-
-    for (const cat of cats) {
-      for (const dog of dogs) {
-        if (
-          !adjList[cat].has(dog) &&
-          !doesCauseCycle(cat, dog) &&
-          !doesCauseCycle(dog, cat)
-        ) {
-          validEdges.push([cat, dog]);
-        }
-      }
-    }
-
-    return validEdges;
-  };
-
-  const getRankings = (): string[][] => {
-    const allNodes = getAllNodes();
-    const cats = allNodes.filter((node) => node[0] === "c");
-    const dogs = allNodes.filter((node) => node[0] === "d");
-    const catsWin = cats.some((node) => adjList[node].size === dogs.length);
-    const dogsWin = dogs.some((node) => adjList[node].size === cats.length);
-    if (catsWin === dogsWin) {
-      console.error(
-        "Something's gone wrong, either both sides have won or neither side has won..."
-      );
-      return [];
-    }
-
-    const orderedCatGroups: string[][] = Array.from(
-      { length: dogs.length + 1 },
-      () => []
-    );
-    cats.forEach((node) => {
-      const index = adjList[node].size;
-      orderedCatGroups[index].push(node);
-    });
-    // In fact this orderedCatGroups now has at index n all cats which are cuter than n dogs
-    const orderedDogGroups: string[][] = Array.from(
-      { length: cats.length + 1 },
-      () => []
-    );
-    dogs.forEach((node) => {
-      const index = adjList[node].size;
-      orderedDogGroups[index].push(node);
-    });
-
-    const mergeArrays = (arr1: string[][], arr2: string[][]): string[][] => {
-      // Assume nonempty arr1 with arr1 having a nonempty final element
-      let result: string[][] = [arr1.pop()!];
-      // Further assume arr1 has at most one more nonempty element than arr2
-      while (arr2.length) {
-        // Get and remove the last non-empty element from arr2
-        let subArr2: string[] = [];
-        while (arr2.length) {
-          let temp = arr2.pop()!;
-          if (temp.length) {
-            subArr2 = temp;
-            break;
-          }
-        }
-        // Add to result if it's not empty
-        if (subArr2.length) {
-          result.push(subArr2);
-        }
-        if (!arr1.length) {
-          break;
-        }
-        let subArr1: string[] = [];
-        while (arr1.length) {
-          let temp = arr1.pop()!;
-          if (temp.length) {
-            subArr1 = temp;
-            break;
-          }
-        }
-        // Add to result if it's not empty
-        if (subArr1.length) {
-          result.push(subArr1);
-        }
-      }
-      return result.reverse();
-    };
-
-    return catsWin
-      ? mergeArrays(orderedCatGroups, orderedDogGroups)
-      : mergeArrays(orderedDogGroups, orderedCatGroups);
+  const getDirectChildren = (node: string): Set<string> => {
+    return adjList[node] || new Set();
   };
 
   return {
     addNode,
     addEdge,
     getAllNodes,
-    getValidComparisons,
-    getRankings,
+    getDirectChildren,
+    doesCauseCycle,
   };
 };
---
+++ src/utils/arrays.ts
@@ -0,0 +1,42 @@
+/**
+ * Merges two arrays of arrays in an alternating fashion, starting with the last element of the first array.
+ * Assumes nonempty arr1 with arr1 having a nonempty final element and arr1 having at most one more nonempty element than arr2.
+ * @param arr1 - First array of arrays
+ * @param arr2 - Second array of arrays
+ * @returns - Merged array of arrays
+ */
+export const alternateMergeArrays = (
+  arr1: string[][],
+  arr2: string[][]
+): string[][] => {
+  let result: string[][] = [arr1.pop()!];
+
+  while (arr2.length) {
+    let subArr2: string[] = [];
+    while (arr2.length) {
+      let temp = arr2.pop()!;
+      if (temp.length) {
+        subArr2 = temp;
+        break;
+      }
+    }
+    if (subArr2.length) {
+      result.push(subArr2);
+    }
+    if (!arr1.length) {
+      break;
+    }
+    let subArr1: string[] = [];
+    while (arr1.length) {
+      let temp = arr1.pop()!;
+      if (temp.length) {
+        subArr1 = temp;
+        break;
+      }
+    }
+    if (subArr1.length) {
+      result.push(subArr1);
+    }
+  }
+  return result.reverse();
+};
