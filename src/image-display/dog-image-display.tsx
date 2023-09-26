6dd032dfb7635c432ff89e1eccc5398c801667e5
Charlie Hewis <54192575+heuuis@users.noreply.github.com>
2023-09-26 18:10:35 +0100

    No api key

--- src/image-display/dog-image-display.tsx
+++ src/image-display/dog-image-display.tsx
@@ -1,8 +1,8 @@
-import { dogApiKey, dogApiUrl } from "../values";
+import { dogApiUrl } from "../values";
 import { AnimalImageDisplay } from "./animal-image-display";
 
 const getDogImagesQuery = (n: number) => {
-  return `${dogApiUrl}/v1/images/search?limit=${n}&api_key=${dogApiKey}`;
+  return `${dogApiUrl}/v1/images/search?limit=${n}`;
 };
 
 export const DogImageDisplay = () => {
--- src/values.ts
+++ src/values.ts
@@ -1,7 +1,3 @@
-export const catApiKey =
-  "live_oWZmpYEWR0WIqJtXXGJrZDYMu7K2Ulmjgy1hEB9pHCTubTxxdbdV6ABT6lJcZKWr";
 export const catApiUrl = "https://api.thecatapi.com";
 
-export const dogApiKey =
-  "live_mrlCAiUTEa97Yw0bIXLgim39NQBmepZBq0tBXZ7vYQwy93wUjE25nyYRuzYNlsch";
 export const dogApiUrl = "https://api.thedogapi.com";
